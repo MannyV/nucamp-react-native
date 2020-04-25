37bf082931dc1be068b6b7f92050a2bd29c24ce9
Emmanuel Vayleux <MannyV@users.noreply.github.com>
2020-04-25 11:04:56 -0400

    Task 1

---
+++ components/AboutComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+export default class About extends Component {
+
+    static navigationOptions = {
+        title: 'About us'
+    }
+
+    render() {
+        return (
+            <ScrollView></ScrollView>
+        )
+    }
+}
---
+++ components/ContactComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { ScrollView } from 'react-native';
+
+export default class Contact extends Component {
+    
+    static navigationOptions = {
+        title: 'Contact us'
+    }
+
+    render() {
+        return (
+            <ScrollView></ScrollView>
+        )
+    }
+}
